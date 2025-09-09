778aeb0d3e479d038a70d323f4f93ad555188541
xfy <i@rua.plus>
2025-09-09 15:01:03 +0800

    fix: http listening addr

--- src/http.rs
+++ src/http.rs
@@ -94,7 +94,7 @@ pub async fn run_http(args: Args) -> anyhow::Result<()> {
 
     let app = logging_route(app);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     info!("listening on {}", listener.local_addr()?);
     axum::serve(
         listener,
